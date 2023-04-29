f93d8d49d9173a10d275e06e8f93ebcc57a22398
Mohammad-Shoaib-1999 <mdmohammad976797@gmail.com>
2023-04-29 16:29:41 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/UserList.js";
-import UserDetails from "./components/UserDetails.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/UserList";
+import UserDetails from "./components/UserDetails";
 import './App.css'; 
 
 function App() {
